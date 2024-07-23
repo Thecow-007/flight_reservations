447b2469325f3dd4326f214a0a876fdd163e7476
Daniel Bierman <biermandaniel65@gmail.com>
2024-07-23 16:53:10 -0400

    Added images and desc to main page

--- lib/main.dart
+++ lib/main.dart
@@ -20,27 +20,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flight Hub',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flight Reservations'),
+      home: const MyHomePage(title: 'Flight Hub'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -86,13 +77,41 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            //Customers + Airplanes Row
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Stack(
+                  children: [
+                    Image.asset("Photos/Customers.png", height:200.0, width:200.0),
+                    Text("Customers")
+                  ],
+                ),
+                Stack(
+                  children: [
+                    Image.asset("Photos/planes.png", height:200.0, width:200.0),
+                    Text("Airplanes")
+                  ],
+                )
+              ],
             ),
-            Text(
-              'something',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Stack(
+                  children: [
+                    Image.asset("Photos/flights.png", height: 200.0, width:200.0),
+                    Text("Flights")
+                  ],
+                ),
+                Stack(
+                  children: [
+                    Image.asset("Photos/Rez.png", height:200.0, width:200.0),
+                    Text("Reservations")
+                  ],
+                )
+              ],
+            )
           ],
         ),
       ),
